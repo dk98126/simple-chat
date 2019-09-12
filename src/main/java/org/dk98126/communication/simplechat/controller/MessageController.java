43d3b9b99d72671da3ce94f195a5bb01fd527956
George Djaniashvili <g.djaniashvili@gmail.com>
2019-09-12 13:43:00 +0300

    initial commit for branch messages

---
+++ src/main/java/org/dk98126/communication/simplechat/controller/MessageController.java
@@ -0,0 +1,55 @@
+package org.dk98126.communication.simplechat.controller;
+
+import org.dk98126.communication.simplechat.domain.Message;
+import org.dk98126.communication.simplechat.repository.MessagesRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MessageController {
+
+    @Autowired
+    private MessagesRepo messagesRepo;
+
+    @GetMapping("/messages")
+    public String messages(Map<String, Object> model){
+        Iterable<Message> messagesIterable = messagesRepo.findAll();
+        model.put("messagesIterable", messagesIterable);
+        return "messages";
+    }
+
+    @PostMapping("/messages")
+    public String add(@RequestParam String text,
+                      @RequestParam String tag,
+                      Map<String, Object> model){
+
+        Message message = new Message(text, tag);
+        messagesRepo.save(message);
+
+        Iterable<Message> messagesIterable = messagesRepo.findAll();
+        model.put("messagesIterable", messagesIterable);
+
+        return "messages";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messagesIterable;
+
+        if (filter != null && !filter.isEmpty()) {
+            messagesIterable = messagesRepo.findByTag(filter);
+        } else {
+            messagesIterable = messagesRepo.findAll();
+        }
+
+        model.put("messagesIterable", messagesIterable);
+
+        return "messages";
+    }
+
+}
---
+++ src/main/java/org/dk98126/communication/simplechat/repository/MessagesRepo.java
@@ -0,0 +1,12 @@
+package org.dk98126.communication.simplechat.repository;
+
+import org.dk98126.communication.simplechat.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessagesRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
