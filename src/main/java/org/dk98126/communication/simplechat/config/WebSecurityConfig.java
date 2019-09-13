85f39563a616265a62eace57e6b3b5063b879913
George Djaniashvili <g.djaniashvili@gmail.com>
2019-09-13 20:26:27 +0300

    Some security changes

--- src/main/java/org/dk98126/communication/simplechat/config/WebSecurityConfig.java
+++ src/main/java/org/dk98126/communication/simplechat/config/WebSecurityConfig.java
@@ -38,9 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                 .and()
                     .logout()
-                    .permitAll()
-                .and()
-                    .csrf().disable();
+                    .permitAll();
     }
 
     @Override
